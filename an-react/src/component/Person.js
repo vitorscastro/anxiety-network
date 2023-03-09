643429b51b8d3bc4997aa2e84c894540058070b3
Vitor Castro <Vitor.Castro@Indellient.com>
2023-03-09 15:29:08 -0500

    Call API and set CORS

--- an-react/src/component/Person.js
+++ an-react/src/component/Person.js
@@ -1,5 +1,19 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 export default function Person() {
+   const url = "http://localhost:8000/test_db_conn";
+   const [personResponse, setPersonResponse] = useState({});
+
+   useEffect(() => {
+      fetch(url)
+         .then(res => res.json())
+         .then(obj => {
+            setPersonResponse(obj);
+         });
+   }, []);
+
    return (
-      <div>person</div>
+      <div>{`person ${personResponse.status}`}</div>
    )
 }
--- py/main.py
+++ py/main.py
@@ -1,9 +1,21 @@
-from contextlib import asynccontextmanager
 from typing import Union
 from fastapi import FastAPI, Depends
 import psycopg2
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+   "http://localhost:3000"
+]
+
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins = origins,
+   allow_credentials = True,
+   allow_methods = ["*"],
+   allow_headers = ["*"],
+)
 
 def setup_db():
    conn = psycopg2.connect(
